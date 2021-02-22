64d72556d001eb4bd5f3cf38525855ff01b1dc73
Donald Ballowe <dballowe7912@gmail.com>
2021-02-22 03:22:47 -0600

    Added chroma and color helper functions to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { Component } from 'react';
 import Palette from './components/Palette/Palette';
-import seedColors from './seedColors';
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]}/>
-    </div>
-  );
+import seedColors from './seedColors';
+import { generatePalette } from "./colorHelpers";
+
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]))
+    return (
+      <div className="App">
+        <Palette {...seedColors[4]}/>
+      </div>
+    );
+  }
 }
 
 export default App;
