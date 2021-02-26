069c973c886da5d8c154a2b57dbb9ce3729c8212
Donald Ballowe <dballowe7912@gmail.com>
2021-02-26 03:37:47 -0600

    Added react-router-dom, switch, and route to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
 
 import seedColors from './seedColors';
-import { generatePalette } from "./colorHelpers";
+import { generatePalette } from './colorHelpers';
+import { FindReplaceTwoTone } from '@material-ui/icons';
 
 class App extends Component {
+  findPalette(id){
+    return seedColors.find(function(palette){
+      return palette.id === id
+    });
+  }
   render() {
-    console.log(generatePalette(seedColors[4]))
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])}/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}
+        />
+      </Switch>
+      // <div className='App'>
+      //   <Palette palette={generatePalette(seedColors[4])}/>
+      // </div>
     );
   }
 }
